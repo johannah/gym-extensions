c5bb2c020d4b6bf8c21ad45bf2b35713aea3c666
johannah <jh1736@gmail.com>
2017-07-24 12:34:49 -0400

    added logging

--- gym_extensions/tests/test_contextual_envs.py
+++ gym_extensions/tests/test_contextual_envs.py
@@ -1,9 +1,9 @@
 import gym
-from gym.envs.classic_control.cartpole import CartPoleEnv
-from gym.envs.classic_control.pendulum import PendulumEnv
+import logging
 from gym_extensions.discrete import classic
 
 def test_cartpole_contextual():
+    from gym.envs.classic_control.cartpole import CartPoleEnv
     env_id = 'CartPoleContextual-v0'
     env = gym.make(env_id)
     if isinstance(env.unwrapped, CartPoleEnv):
@@ -14,20 +14,23 @@ def test_cartpole_contextual():
     nr_of_items_context_space_info = 10
     nr_unwrapped = len(list(env.unwrapped.context_space_info().keys()))
     if nr_of_items_context_space_info != nr_unwrapped:
-        print('context_space_info() function needs to be implemented!')
+        logging.info('context_space_info() function needs to be implemented!')
         raise NotImplementedError
 
     context_vect = [0.01, 0.01, 0.01, 0.01]
-    # these should change because change_context_function
     if context_vect == env.unwrapped.context:
+        logging.info("context was not initialized correctly")
         raise AttributeError
 
     env.unwrapped.change_context(context_vect)
+    # these should change because change_context_function
     if context_vect != env.unwrapped.context:
+        logging.info("context was not changed by change_context")
         raise AttributeError
 
 
 def test_pendulum_contextual():
+    from gym.envs.classic_control.pendulum import PendulumEnv
     env_id = 'PendulumContextual-v0'
     env = gym.make(env_id)
     if isinstance(env.unwrapped, PendulumEnv):
@@ -38,15 +41,17 @@ def test_pendulum_contextual():
     nr_of_items_context_space_info = 10
     nr_unwrapped = len(list(env.unwrapped.context_space_info().keys()))
     if nr_of_items_context_space_info != nr_unwrapped:
-        print('context_space_info() function needs to be implemented!')
+        logging.info('context_space_info() function needs to be implemented!')
         raise NotImplementedError
 
     context_vect = [0.01, 0.01]
     if context_vect == env.unwrapped.context:
+        logging.info("context was not initialized correctly")
         raise AttributeError
 
     env.unwrapped.change_context(context_vect)
     if context_vect != env.unwrapped.context:
+        logging.info("context was not changed by change_context")
         raise AttributeError
 
 
